6882f98f76fce7ff01843ef912374c45e9a7a618
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2023-11-04 01:45:29 +0000

    Implement insertion sort algorythm

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,24 @@
+'''
+Insertion sort algorythm
+'''
 
+def insertion_sort(arr):
+    '''
+    Sort a given array
+    '''
+
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j] < arr[j-1] and j > 0:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
+
+numbers = [9, 3, 6, 1, 8, 4, 5, 2]
+
+words = ['car', 'plane', 'bus', 'tractor', 'cart']
+
+insertion_sort(numbers)
+insertion_sort(words)
+
+print(numbers)
+print(words)
